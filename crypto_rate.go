a94bee82fa57ec49880e6c59ca90c7baf1f1f739
ib407ov <iliuha.banik@gmail.com>
2024-02-16 14:25:52 +0200

    BugFix

--- crypto_rate.go
+++ crypto_rate.go
@@ -15,33 +15,56 @@ type CryptoCompare struct {
 
 type CryptoCompareResponse map[string]CryptoCompare
 
-func GetDataCurrencyRate(crypto []string) (CryptoCompareResponse, error) {
-	countGoroutines := len(crypto)/25 + 1
-	fmt.Println(countGoroutines)
-	cryptoRow := strings.Join(crypto, ",")
+type CryptoCompareResponseStruct struct {
+	Data []CryptoCompare
+}
+
+func GetDataCurrencyRate(crypto []string) (CryptoCompareResponseStruct, error) {
 	var result CryptoCompareResponse
+	var dataStruct CryptoCompareResponseStruct
 
-	//Get API from URL
-	url := fmt.Sprintf("https://min-api.cryptocompare.com/data/pricemulti?fsyms=%s&tsyms=USD", cryptoRow)
-	resp, err := http.Get(url)
-	defer resp.Body.Close()
-	if err != nil {
-		return result, err
+	indexA := 0
+	indexB := 25
+	for i := 0; i < len(crypto)/25+1; i++ {
+		if indexB > (len(crypto)) {
+			indexB = len(crypto)
+		}
+
+		newCrypto := crypto[indexA:indexB]
+		newCryptoStrig := strings.Join(newCrypto, ",")
+
+		//Get API from URL
+		url := fmt.Sprintf("https://min-api.cryptocompare.com/data/pricemulti?fsyms=%s&tsyms=USD", newCryptoStrig)
+		resp, err := http.Get(url)
+		defer resp.Body.Close()
+		if err != nil {
+			return CryptoCompareResponseStruct{}, err
+		}
+
+		//API -> body
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Помилка при читанні відповіді:", err)
+			return CryptoCompareResponseStruct{}, err
+		}
+
+		//Unmarshal body
+		err = json.Unmarshal(body, &result)
+		if err != nil {
+			fmt.Println("Помилка при розборі JSON:", err)
+			return CryptoCompareResponseStruct{}, err
+		}
+
+		for key, val := range result {
+			dataStruct.Data = append(dataStruct.Data, CryptoCompare{
+				Symbol: key,
+				Price:  val.Price,
+			})
+		}
+
+		indexA += 25
+		indexB += 25
 	}
 
-	//API in body
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Помилка при читанні відповіді:", err)
-		return result, err
-	}
-
-	//Unmarshal body
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		fmt.Println("Помилка при розборі JSON:", err)
-		return result, err
-	}
-
-	return result, nil
+	return dataStruct, nil
 }
